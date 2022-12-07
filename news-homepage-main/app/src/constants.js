44c679ca042ebfbab3c0fea6b96180b56c060cd9
MustafaWael <mostafawael891@gmail.com>
2022-12-06 16:56:56 -0800

    add constants file for all static data

---
+++ news-homepage-main/app/src/constants.js
@@ -0,0 +1,18 @@
+export const navigationLinks = [
+  {
+    id: 1,
+    text: "Home",
+  },
+  {
+    id: 2,
+    text: "Popular",
+  },
+  {
+    id: 3,
+    text: "Trending",
+  },
+  {
+    id: 4,
+    text: "Categories",
+  },
+];
