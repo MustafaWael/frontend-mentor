68372cd3797865331c2b6b96e6250bf0d460f537
MustafaWael <mostafawael891@gmail.com>
2022-12-16 00:00:33 +0200

    add Footer component

---
+++ news-homepage-main/app/src/Components/Footer.jsx
@@ -0,0 +1,24 @@
+export const Footer = () => {
+  return (
+    <footer className="px-4 py-10 flex justify-center">
+      <div className="text-sm font-Inter">
+        Challenge by{" "}
+        <a
+          className="text-soft_orange font-bold"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          className="text-soft_red font-bold"
+          href="https://www.twitter.com/@_mustafa_wael_"
+        >
+          @Mustafa Wael
+        </a>
+        .
+      </div>
+    </footer>
+  );
+};
