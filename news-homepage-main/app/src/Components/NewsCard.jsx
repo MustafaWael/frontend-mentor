cf0c54e6298b18e2e78689435476f6f347a440b1
MustafaWael <mostafawael891@gmail.com>
2022-12-16 00:23:05 +0200

    fix NewsCard items gap in small screen

--- news-homepage-main/app/src/Components/NewsCard.jsx
+++ news-homepage-main/app/src/Components/NewsCard.jsx
@@ -2,7 +2,7 @@ export const NewsCard = ({ news }) => {
   return (
     <div className="bg-very_dark_blue px-5 py-6 h-full flex flex-col">
       <h2 className="font-Inter font-bold text-3xl text-soft_orange mb-8">New</h2>
-      <ul className="h-full flex flex-col justify-between">
+      <ul className="h-full flex flex-col justify-between gap-y-10">
         {news.map(({ id, title, subTitle }) => (
           <ListItem key={id} title={title} subTitle={subTitle} />
         ))}
@@ -13,7 +13,7 @@ export const NewsCard = ({ news }) => {
 
 const ListItem = ({ title, subTitle }) => {
   return (
-    <li className="[&:not(:last-child)]:pb-7 xl:[&:not(:last-child)]:pb-14 [&:not(:last-child)]:border-b-[1px] border-dark_grayish_blue">
+    <li className="[&:not(:last-child)]:pb-10 xl:[&:not(:last-child)]:pb-14 [&:not(:last-child)]:border-b-[1px] border-dark_grayish_blue">
       <h3 className="font-Inter font-bold text-lg text-off_white hover:text-soft_orange cursor-pointer">{title}</h3>
       <p className="text-grayish_blue">{subTitle}</p>
     </li>
