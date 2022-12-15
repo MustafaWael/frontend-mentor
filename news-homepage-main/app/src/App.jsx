9a9a3e5ba9ce6e29592b9242214dd27cc5a13653
MustafaWael <mostafawael891@gmail.com>
2022-12-15 23:52:35 +0200

    fix scrolling after MobileMenu opened

--- news-homepage-main/app/src/App.jsx
+++ news-homepage-main/app/src/App.jsx
@@ -4,12 +4,20 @@ import { Navigation } from "./Components/Navigation";
 
 function App() {
   const [isMobileMenuOpend, setIsMobileMenuOpend] = useState(false);
+
+  const mobileMenuHandler = (val) => {
+    val
+      ? (document.documentElement.style.overflow = "hidden")
+      : (document.documentElement.style.overflow = "auto");
+    setIsMobileMenuOpend(val);
+  };
+
   return (
     <>
-      <Navigation setIsMobileMenuOpend={setIsMobileMenuOpend} />
+      <Navigation setIsMobileMenuOpend={mobileMenuHandler} />
       <MobileMenu
         isMobileMenuOpend={isMobileMenuOpend}
-        setIsMobileMenuOpend={setIsMobileMenuOpend}
+        setIsMobileMenuOpend={mobileMenuHandler}
       />
     </>
   );
