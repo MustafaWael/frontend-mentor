eff882eb878fc523da6774648cf50d8f1ea7c22a
MustafaWael <mostafawael891@gmail.com>
2022-12-16 16:31:14 +0200

    fix image height in small screen

--- news-homepage-main/app/src/Components/FeaturedBlogs.jsx
+++ news-homepage-main/app/src/Components/FeaturedBlogs.jsx
@@ -8,13 +8,13 @@ export const FeaturedBlogs = ({ blogs }) => (
 
 const BlogItem = ({ no, title, subTitle, image }) => (
   <li className="flex gap-x-5">
-    <div className="h-full">
-      <img className="w-[110px] max-w-[110px] h-full object-cover object-center" src={image.src} alt={image.alt} />
+    <div className="max-h-full">
+      <img className="w-[110px] max-w-[110px] h-full min-h-full object-cover object-center" src={image.src} alt={image.alt} />
     </div>
-    <div className="flex flex-col justify-around gap-y-4">
-      <span className="font-Inter font-bold text-2xl text-grayish_blue">{no}</span>
+    <div className="flex flex-col justify-around gap-3">
+      <span className="font-Inter font-bold text-3xl text-grayish_blue">{no}</span>
       <h2 className="font-Inter font-bolder text-lg text-very_dark_blue hover:text-soft_red cursor-pointer">{title}</h2>
-      <p className="font-Inter text-paragraph text-dark_grayish_blue">{subTitle}</p>
+      <p className="font-Inter text-sm text-dark_grayish_blue">{subTitle}</p>
     </div>
   </li>
 );
