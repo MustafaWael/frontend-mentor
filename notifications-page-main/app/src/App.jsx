75a866646f07180f459fa3332700b3ba66dd0fa8
MustafaWael <mostafawael891@gmail.com>
2022-12-20 01:52:35 +0200

    add NotificationList component to App

--- notifications-page-main/app/src/App.jsx
+++ notifications-page-main/app/src/App.jsx
@@ -1,5 +1,12 @@
+import { NotificationList } from "./components/NotificationList";
+import { notifications } from "./constants";
+
 function App() {
-  return <p className="font-Plus-Jakarta font-bold text-8xl">app</p>;
+  return (
+    <main className="bg-very_light_grayish_blue md:py-14 mx-auto flex flex-col justify-center items-center min-h-screen">
+      <NotificationList notifications={notifications} />
+    </main>
+  );
 }
 
 export default App;
