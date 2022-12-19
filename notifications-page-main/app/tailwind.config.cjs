78471e09359dd280e2ebfb58beefbeeadd7e90ee
MustafaWael <mostafawael891@gmail.com>
2022-12-20 01:46:40 +0200

    add very_light_grayish_blue color

--- notifications-page-main/app/tailwind.config.cjs
+++ notifications-page-main/app/tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       red: "hsl(1, 90%, 64%)",
       blue: "hsl(219, 85%, 26%)",
       White: "hsl(0, 0%, 100%)",
+      very_light_grayish_blue: "hsl(210, 60%, 98%)",
       light_grayish_blue_1: "hsl(211, 68%, 94%)",
       light_grayish_blue_2: "hsl(205, 33%, 90%)",
       grayish_blue: "hsl(219, 14%, 63%)",
