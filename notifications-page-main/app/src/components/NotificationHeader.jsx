5821c7205ab5576306741e44a8e9cfde378b81c4
MustafaWael <mostafawael891@gmail.com>
2022-12-21 11:06:01 +0200

    Move notification header to NotificationHeader Component

---
+++ notifications-page-main/app/src/components/NotificationHeader.jsx
@@ -0,0 +1,34 @@
+export const NotificationHeader = ({
+  notificationList,
+  setNotificationList,
+}) => {
+  let notMarkedCount = notificationList.filter(
+    (item) => !item.hasBeenRead
+  ).length;
+
+  const markAllAsRead = () => {
+    setNotificationList((current) =>
+      current.map((item) => ({ ...item, hasBeenRead: true }))
+    );
+  };
+
+  return (
+    <div className="flex justify-between items-center font-Plus-Jakarta mb-5">
+      <div className="flex gap-x-2 items-center">
+        <p className="text-very_dark_blue font-bolder text-2xl">
+          Notifications
+        </p>
+        <span className="inline-block bg-blue text-White px-[12px] py-[2px] rounded-md text-md font-bold h-fit">
+          {notMarkedCount}
+        </span>
+      </div>
+      <button
+        className="text-md font-bold text-dark_grayish_blue hover:text-blue"
+        onClick={markAllAsRead}
+        disabled={!notMarkedCount}
+      >
+        Mark all as read
+      </button>
+    </div>
+  );
+};
--- notifications-page-main/app/src/components/NotificationList.jsx
+++ notifications-page-main/app/src/components/NotificationList.jsx
@@ -1,34 +1,17 @@
 import { useState } from "react";
 import { Notification } from "./Notification";
+import { NotificationHeader } from "./NotificationHeader";
 
 export const NotificationList = ({ notifications }) => {
   const [notificationList, setNotificationList] = useState(() => notifications);
-  let notMarkedCount = notificationList.filter(
-    (item) => !item.hasBeenRead
-  ).length;
-
-  const markAllAsRead = () => {
-    setNotificationList((current) =>
-      current.map((item) => ({ ...item, hasBeenRead: true }))
-    );
-  };
 
   return (
     <article className="bg-White rounded-lg shadow-[0_0_30px_-3px_hsl(211,68%,94%)] p-4 max-w-[620px]">
-      <div className="flex justify-between items-center font-Plus-Jakarta mb-5">
-        <div className="flex gap-x-2 items-center">
-          <p className="text-very_dark_blue font-bolder text-2xl">
-            Notifications
-          </p>
-          <span className="inline-block bg-blue text-White px-[12px] py-[2px] rounded-md text-md font-bold h-fit">
-            {notMarkedCount}
-          </span>
-        </div>
-        <button className="text-md font-bold text-dark_grayish_blue hover:text-blue" onClick={markAllAsRead} disabled={!notMarkedCount}>
-          Mark all as read
-        </button>
-      </div>
-      
+      <NotificationHeader
+        notificationList={notificationList}
+        setNotificationList={setNotificationList}
+      />
+
       <ul className="flex flex-col gap-3">
         {notificationList.map(({ id, user, action, date, hasBeenRead }) => {
           return (
