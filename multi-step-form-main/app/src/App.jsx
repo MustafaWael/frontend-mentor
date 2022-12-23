534663152e889db0631fa12e21df65e98efed2a1
MustafaWael <mostafawael891@gmail.com>
2022-12-24 01:28:29 +0200

    New challenge | Multi-step form 🚀

---
+++ multi-step-form-main/app/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="text-5xl font-Ubuntu">App</p>;
+}
+
+export default App;
---
+++ multi-step-form-main/app/tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      marine_blue: "hsl(213, 96%, 18%)",
+      purplish_blue: "hsl(243, 100%, 62%)",
+      pastel_blue: "hsl(228, 100%, 84%)",
+      light_blue: "hsl(206, 94%, 87%)",
+      strawberry_red: "hsl(354, 84%, 57%)",
+    },
+    fontFamily: {
+      Ubuntu: "Ubuntu",
+    },
+    fontWeight: {
+      regular: "400",
+      medium: "500",
+      bold: "700",
+    },
+    extend: {
+      screens: {
+        Mobile: "375px",
+        Desktop: "1440px",
+      },
+      fontSize: {
+        paragraph: "16px",
+      },
+    },
+  },
+  plugins: [],
+};
